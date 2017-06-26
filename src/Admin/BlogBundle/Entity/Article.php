4e1e31416aa4fe7278699ae5b6c9edd1e9396bdd
Alexandre Peyron <alpeyron@gmail.com>
2017-06-26 07:32:18 +0200

    add Slug on Article

--- src/Admin/BlogBundle/Entity/Article.php
+++ src/Admin/BlogBundle/Entity/Article.php
@@ -2,6 +2,7 @@
 
 namespace Admin\BlogBundle\Entity;
 
+use Gedmo\Mapping\Annotation as Gedmo;
 use Doctrine\ORM\Mapping as ORM;
 use Doctrine\Common\Collections\ArrayCollection;
 
@@ -28,6 +29,14 @@ class Article
      * @ORM\Column(name="title", type="string", length=255, unique=true)
      */
     private $title;
+
+    /**
+     * @var string
+     *
+     * @Gedmo\Slug(fields={"title"})
+     * @ORM\Column(name="slug", type="string", length=255, unique=true)
+     */
+    private $slug;
 
     /**
      * @var string
@@ -111,6 +120,30 @@ class Article
     public function getTitle()
     {
         return $this->title;
+    }
+
+    /**
+     * Set slug
+     *
+     * @param string $slug
+     *
+     * @return Article
+     */
+    public function setSlug($slug)
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    /**
+     * Get slug
+     *
+     * @return string
+     */
+    public function getSlug()
+    {
+        return $this->slug;
     }
 
     /**
