9abcfc73afe00ab2b5cb5f2b6355fc72773e2626
Alexandre Peyron <alpeyron@gmail.com>
2017-09-20 17:27:36 +0200

    clean code + doc

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -3,17 +3,19 @@
 namespace AppBundle\Controller;
 
 use Admin\BlogBundle\Entity\Article;
-use Admin\BlogBundle\Repository\ArticleRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
     /**
+     * Homepage
+     *
      * @Route("/", name="homepage")
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function indexAction(Request $request)
+    public function indexAction()
     {
         $em = $this->getDoctrine()->getManager();
 
@@ -26,15 +28,24 @@ class DefaultController extends Controller
     }
 
     /**
+     * Page About
+     *
      * @Route("/about", name="about")
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function aboutAction(Request $request)
+    public function aboutAction()
     {
         return $this->render('default/about.html.twig');
     }
 
     /**
+     * Page for Article
+     *
      * @Route("/article/{slug}", name="front_article_show")
+     *
+     * @param Article $article An article instance directly request by SF
+     * @return \Symfony\Component\HttpFoundation\Response
      */
     public function showArticleAction(Article $article)
     {
